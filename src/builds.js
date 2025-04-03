bb2fcb5aaf7e1a14f1e18ee1ae3971dcf0a9a764
Ian Dees <ian.dees@gmail.com>
2025-04-03 00:44:32 -0500

    Use a newer upload-pages-artifact action (#62)
    * Use a newer upload-pages-artifact action
    
    * Add pr previews
    
    * Update dependencies
    
    * Add package-lock.json
    
    * Wrap in an async, reformat
    
    * Don't need to export this
    
    * Update Gemfile dependencies
    
    * Remove the Jekyll stuff
    
    * Copy stuff to _site
    
    * Include the javascript bundle
    
    * Does v2 work?
    
    * Looks like v4 is required
    
    * Include css
    
    * Include the scale.fix.js
    
    * Include image
    
    * Images are inside /assets
    
    * Recover the css better
    
    * Add fonts
    
    * Clean up font-face in css a bit
    
    * Add links to the other pieces of the page
    
    * Improve javascript situation?
    
    * Point to the correct bundle
    
    * Trying to make the builds.html page work
    
    * Build CSS using esbuild
    
    * Use the built css
    
    * Missed the 'min'
    
    * Fix builds css
    
    * Fixing typos in css
    
    * Fixes builds.html
    
    * Update spiders page
    
    * This page uses datatable too
    
    * Update wikidata page
    
    * Clean up the map too
    
    * Move map up one level and build style json into it
    
    * Add PR preview map

---
+++ src/builds.js
@@ -0,0 +1,99 @@
+import {fetchHistoryList} from './shared.js';
+import $ from "jquery";
+import DataTable from 'datatables.net-dt';
+
+function formatBytes(bytes, decimals = 2) {
+    if (bytes === 0) return '0 Bytes';
+
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+
+(async function () {
+    const data = await fetchHistoryList();
+
+// Render with datatable
+    let dataTable = $("#spider-table").DataTable({
+        data,
+        lengthMenu: [
+            [10, 15, 20, 25, 50, 75, 100, -1],
+            [10, 15, 20, 25, 50, 75, 100, "All"],
+        ],
+        pageLength: 15,
+        order: [[0, 'desc']],
+        columns: [
+            {"title": "Run", "data": "name"},
+            {
+                "title": "Run statistics",
+                "data": "stats_url",
+                "render": function (data, type, row, meta) {
+                    if (type === 'display') {
+                        if (data) {
+                            return '<a href="' + data + '">Summary</a>'
+                        }
+                        return ""
+                    }
+                    return data;
+                }
+            },
+            {
+                "title": "Spider count",
+                "data": "spiders",
+                "render": function (data, type, row, meta) {
+                    if (type === 'display' && data) {
+                        data = data.toLocaleString("us-US")
+                    }
+                    return data;
+                }
+            },
+            {
+                "title": "Feature count",
+                "data": "total_lines",
+                "render": function (data, type, row, meta) {
+                    if (type === 'display' && data) {
+                        data = data.toLocaleString("us-US")
+                    }
+                    return data;
+                }
+            },
+            {
+                "title": "Download features",
+                "data": "size_bytes",
+                "render": function (data, type, row, meta) {
+                    if (type === 'display') {
+                        if (data && (data > 0)) {
+                            let text = "Download " + formatBytes(data)
+                            return '<a href="' + row["output_url"] + '">' + text + '</a>'
+                        }
+                        return ""
+                    }
+                    return data;
+                }
+            },
+        ],
+        columnDefs: [
+            {className: 'dt-center', targets: [1, 4]},
+            {className: 'dt-right', targets: [2, 3]},
+        ],
+        "footerCallback": function (row, data, start, end, display) {
+            let api = this.api()
+            // Total up the numeric columns (all next to each)
+            let columns = [3]
+            for (let i in columns) {
+                let total = api
+                    .column(columns[i], {filter: "applied"})
+                    .data()
+                    .reduce(function (a, b) {
+                        return a + b;
+                    }, 0);
+                // Update total in footer
+                $(api.column(columns[i]).footer()).html(total.toLocaleString("us-US"));
+            }
+        },
+    });
+})();
---
+++ src/wikidata.js
@@ -0,0 +1,178 @@
+import {fetchHistoryList} from './shared.js';
+import $ from "jquery";
+import DataTable from 'datatables.net-dt';
+
+function isInt(value) {
+    if (isNaN(value)) {
+        return false;
+    }
+    let x = parseFloat(value);
+    return (x | 0) === x;
+}
+
+(async function () {
+
+    async function loadInsights(url) {
+        dataTable.clear()
+        dataTable.draw()
+        const insightsResponse = await window.fetch(url)
+        const insightsJson = await insightsResponse.json()
+        let data = insightsJson["data"]
+        if (!("atp_country_count" in data[0])) {
+            // If we encounter an insights JSON without the newer fields then we add them dynamically
+            // so that the new DataTables config does not error i.e. backwards compatability for now.
+            for (let i = 0; i < data.length; i++) {
+                data[i]["atp_country_count"] = ""
+                data[i]["atp_supplier_count"] = ""
+            }
+        }
+        else {
+            // Re-calculate the "atp_count" (number of POIs) based on the sum across all countries
+            // of the largest contributing spider in each area.
+            // It might be that we move this calculation to the "insights" command at some point
+            // but for now as it is here we preserve backwards compatibility.
+            for (let i = 0; i < data.length; i++) {
+                if (data[i]["atp_count"]) {
+                    let new_atp_count = 0
+                    Object.keys(data[i]["atp_splits"]).forEach(function (country) {
+                        let country_max = 0
+                        Object.keys(data[i]["atp_splits"][country]).forEach(function (spider) {
+                            let count = data[i]["atp_splits"][country][spider]
+                            if (count > country_max) {
+                                country_max = count
+                            }
+                        })
+                        new_atp_count += country_max
+                    })
+                    data[i]["atp_count"] = new_atp_count
+                }
+            }
+        }
+        // Old insights.json had attributes "nsi_brand" and "nsi_description".
+        // These were misnamed and should in fact be references to Wikidata values.
+        // Fix these, leaving an "nsi_brand" field which will be incorrect but
+        // will be good in later insights.json.
+        if ("nsi_description" in data[0]) {
+            for (let i = 0; i < data.length; i++) {
+                data[i]["q_title"] = data[i]["nsi_brand"]
+                data[i]["q_description"] = data[i]["nsi_description"]
+            }
+        }
+        dataTable.rows.add(insightsJson["data"])
+        dataTable.draw()
+    }
+
+    function newInsightsJsonSelected() {
+        loadInsights(document.getElementById('insight-select').value)
+    }
+
+    const historyList = await fetchHistoryList()
+
+    let dataTable = $("#spider-table").DataTable({
+        lengthMenu: [10, 15, 20, 25, 50, 75, 100],
+        pageLength: 10,
+        dom: 'l<"insight-files">frtip',
+        order: [[1, 'desc']],
+        columnDefs: [
+            { className: 'dt-center', targets: [0,5,6,7] },
+            { className: 'dt-right', targets: [1,2,3,4] },
+        ],
+        columns: [
+            {
+                "title": "Q-code",
+                "data": "code",
+                "render": function (data, type, row, meta) {
+                    if (type === 'display' && data.startsWith("Q")) {
+                        data = '<a href="https://www.wikidata.org/wiki/' + data + '">' + data + '</a>';
+                    }
+                    return data;
+                }
+            },
+            {"title": "OSM count", "data": "osm_count"},
+            {"title": "ATP count", "data": "atp_count"},
+            {
+                "title": "Countries",
+                "data": "atp_country_count",
+                "render": function (data, type, row, meta) {
+                    if (type === 'display' && row["atp_country_count"]) {
+                        let dataUrl = URL.createObjectURL(
+                            new Blob([JSON.stringify(row["atp_splits"], null, 6)], { type: "application/json" })
+                        );
+                        data = "<a target='_blank' href='"+dataUrl+"'>" + data + "</a>";
+                    }
+                    return data;
+                }
+            },
+            {"title": "Spiders", "data": "atp_supplier_count"},
+            {"title": "ATP brand", "data": "atp_brand"},
+            {
+                "title": "NSI brand",
+                "data": "nsi_brand",
+                "render": function (data, type, row, meta) {
+                    if (type === 'display' && data) {
+                        data = '<a href="https://nsi.guide/map.html?t=brands&tt=' + row["code"] + '">' + data + '</a>';
+                    }
+                    return data;
+                }
+            },
+            {"title": "Q-title", "data": "q_title"},
+            {
+                "title": "Q-description",
+                "data": "q_description",
+                "render": function (data, type, row, meta) {
+                    if (type === 'display') {
+                        data = '<a href="https://www.wikidata.org/wiki/' + row["code"] + '">' + data + '</a>';
+                    }
+                    return data;
+                }
+            },
+        ],
+        "footerCallback": function (row, data, start, end, display) {
+            let api = this.api()
+            // Total up the numeric columns (all next to each)
+            let columns = [1,2,3,4]
+            for (let i in columns) {
+                let total = api
+                    .column(columns[i], {filter: "applied"})
+                    .data()
+                    .reduce(function (a, b) {
+                        return a + b;
+                    }, 0);
+                // Update total in footer
+                $(api.column(columns[i]).footer()).html(total.toLocaleString("us-US"));
+            }
+        },
+        "createdRow": function (row, data, dataIndex, cells) {
+            // TODO: have an issue column and mark it if anything triggers in this method?
+            if (!data['code'].startsWith("Q")) {
+                // For sure we do not have a valid wikidata Q-code, highlight this line.
+                $(row).css('background-color', 'Pink')
+            }
+            if (isInt(data['atp_count']) && data['osm_count'] > data['atp_count']) {
+                // More data in OSM than ATP, highlight this line and the ATP count cell.
+                $(cells[2]).css('color', 'Red')
+                $(row).css('background-color', 'Pink')
+            }
+            if (data['atp_brand'] && data['nsi_brand'] && !data['nsi_brand'].startsWith(data['atp_brand'])) {
+                // ATP brand name is only allowed to be a strict or leading match of the NSI brand name
+                // If not then highlight the ATP brand name cell and the whole line.
+                $(cells[5]).css('color', 'Red')
+                $(row).css('background-color', 'Pink')
+            }
+            if (data['atp_count'] > 0 && !data['nsi_brand']) {
+                // Something is in ATP but not in NSI, if it's valid then perhaps add to NSI?
+                // Highlight the line as worthy of possible attention.
+                $(row).css('background-color', 'Pink')
+            }
+        },
+    });
+
+    $('div.insight-files').html('<label class="insight-label">Select <select id="insight-select" aria-controls="spider-table" class></select> run</label>');
+    let selectElement = document.getElementById('insight-select');
+    for (let i = 0; i < historyList.length; i++) {
+        selectElement.add(new Option(historyList[i]["name"], historyList[i]["insights_url"]));
+    }
+    selectElement.onchange = newInsightsJsonSelected;
+    // Select the most recent run to display initially.
+    await loadInsights(historyList[0]["insights_url"])
+})();
